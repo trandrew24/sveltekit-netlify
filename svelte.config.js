07f92cbdcc783f2d817dcfcec03860740e55a256
Tran, Andrew <andrew.tran@gentex.com>
2023-07-14 11:28:33 -0400

    update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,19 @@
 import adapter from '@sveltejs/adapter-netlify';
 const config = {
 	kit: {
-		target: '#svelte',
-		adapter: adapter()
+		// target: '#svelte',
+		// adapter: adapter()
+
+		adapter: adapter({
+            // if true, will create a Netlify Edge Function rather
+            // than using standard Node-based functions
+            edge: false,
+
+            // if true, will split your app into multiple functions
+            // instead of creating a single one for the entire app.
+            // if `edge` is true, this option cannot be used
+            split: false
+        })
 	}
 };
 
